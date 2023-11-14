afcfe242cbfaf8c988ab1dc881f96922040abc75
Rochelle Mullen <b00156311@mytudublin.ie>
2023-11-14 14:39:38 +0000

    rm

--- Hungry Dog alpha/Assets/Scripts/PlayerController.cs
+++ Hungry Dog alpha/Assets/Scripts/PlayerController.cs
@@ -1,45 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
-    public boolean IsOnGround = true;
-    public boolean gameOver = false;
+    public bool IsOnGround = true;
+    public bool gameOver = false;
+
+    public AudioClip jump;
+    public AudioClip bark;
+    public AudioClip crash;
+    public AudioSource player;
+    private SpawnManager spawnManager;
+    private Background background;
+    private float repeatFast;
+    public bool hasPower;
+    public int powerDuration = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        player = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround && !gameOver ){
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            player.PlayOneShot(jump, 1.0f);
+            player.PlayOneShot(bark, 1.0f);
+            IsOnGround = false;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround ){
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
-        }
+        
     }
-    private void OnCollisionEnter(Collision collision){
-        isOnGround = true;
-    }
+    
 
     private void OnCollisionEnter(Collision collision){
         if (collision.gameObject.CompareTag("Ground")){
-        isOnGround = true;
+            IsOnGround = true;
    }
-   else if (collision.gameObject.CompareTag("Obstacle")){
-       gameOver = true;
-       Debug.Log("Game Over");
-   }
+       else if (collision.gameObject.CompareTag("Fence")){
+           gameOver = true;
+           Debug.Log("Game Over");
+           player.PlayOneShot(crash, 1.0f);
+
+
+
+        }
+       else if (collision.gameObject.CompareTag("Bone"))
+        {
+            player.PlayOneShot(bark, 1.0f);
+           
+        }
+        else if (collision.gameObject.CompareTag("Ball"))
+        {
+            player.PlayOneShot(bark, 1.0f);
+            
+
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball")){
+            Destroy(other.gameObject);
+            hasPower = true;
+            repeatFast = GetComponent<BoxCollider>().size.x / 4;
+            float delaySpawn = .25f;
+            float interval = .5f;
+            background.moveBackground(repeatFast);
+            spawnManager.InvokeRepeating("SpawnFoodObjects", delaySpawn, interval);
+            spawnManager.InvokeRepeating("SpawnObstacle", delaySpawn, interval);
+            StartCoroutine(PowerUpCool());
+
+        }
+    }
+    IEnumerator PowerUpCool()
+    {
+        yield return new WaitForSeconds(powerDuration);
+        hasPower = false;
     }
 
 }
--- Hungry Dog alpha/Assets/Scripts/SpawnManager.cs
+++ Hungry Dog alpha/Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject fencePrefab;
+    public GameObject[] foodPrefab;
+    private float delay = 2;
+    private float intervalSpawn = 1.5f;
+    private PlayerController playerControllerS;
+    private Vector3 spawnFoodLocation;
+    private Vector3 spawnFenceLocation;
+    public PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnFoodObjects", delay, intervalSpawn);
+        InvokeRepeating("SpawnObstacle", delay, intervalSpawn);
+        playerControllerS = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void SpawnFoodObjects()
+    {
+        spawnFoodLocation = new Vector3(30, Random.Range(5, 20), 0);
+        int index = Random.Range(0, foodPrefab.Length);
+       
+        if (!playerControllerS.gameOver)
+        {
+            Instantiate(foodPrefab[index], spawnFoodLocation, foodPrefab[index].transform.rotation);
+        }
+        
+
+        
+
+    }
+    void SpawnObstacle()
+    {
+        spawnFenceLocation = new Vector3(25, 0, 0);
+        if (!playerControllerS.gameOver)
+        {
+            Instantiate(fencePrefab, spawnFenceLocation, fencePrefab.transform.rotation);
+        }
     }
 }
