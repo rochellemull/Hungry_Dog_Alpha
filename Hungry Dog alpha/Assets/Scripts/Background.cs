92b3741d27815d83c6e53e8882de77eef4fbb372
Rochelle Mullen <b00156311@mytudublin.ie>
2023-11-14 10:50:49 +0000

    initial rm

---
+++ Hungry Dog alpha/Assets/Scripts/Background.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 start;
+    private float repeat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start = transform.position;
+        start = transform.position;
+        repeat = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < (start.x - repeat))
+        {
+            transform.position = start;
+        }
+    }
+    
+}
