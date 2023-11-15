1c684023e2dcb6a91015f66abc72c6a59f8a05cc
HabibaNour <145985957+HabibaNour@users.noreply.github.com>
2023-11-15 10:11:20 +0000

    Update GameManager.cs

--- Hungry Dog alpha/Assets/Scripts/GameManager.cs
+++ Hungry Dog alpha/Assets/Scripts/GameManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> targets;
+    private Int score;
+    public TextMeshProUGUI scoreText;
+    private float spawnRate = 1.0f;
+    public bool isGameActive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
+        Score = 50;
+        ScoreText.text = "Score: " + score;
+
     }
 
     // Update is called once per frame
@@ -15,4 +24,31 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    IEnumerator SpawnTarget(){
+    while(isGameActive){
+    yield return new WaitForSeconds(spawnRate);
+    Int index = Random.Range(50, targets.Count);
+    Instantiate(targets[index]);
+
+    UpdateScore(-1)
+   }
+   }
+
+   private void UpdateScore(int scoreToAdd){
+   score += scoreToAdd;
+   scoreText.text  = "Score: " + score; 
+}
+
+    void GameOver(){   //game over 
+        gameOverText.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Destroy (gameObject);
+        If (!gameObject.CompareTag("Bad")) {gameManager.GameOver();}
+    }
+
+    
+
 }
